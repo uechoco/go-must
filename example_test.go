31dbcc8841bfddf6aa00a7c4a6a7985473c60320
uechoco <y-ueno@mercari.com>
2024-08-15 15:44:45 +0900

    fix package of example_test

--- example_test.go
+++ example_test.go
@@ -1,19 +1,21 @@
-package must
+package must_test
 
 import (
 	"fmt"
 	"math/big"
 	"time"
+
+	"github.com/uechoco/go-must"
 )
 
 func Example_timeParse() {
-	tm := Get2(time.Parse(time.RFC3339, "2020-01-01T11:22:33Z"))
+	tm := must.Get2(time.Parse(time.RFC3339, "2020-01-01T11:22:33Z"))
 	fmt.Println(tm)
 	// Output: 2020-01-01 11:22:33 +0000 UTC
 }
 
 func Example_parseFloat() {
-	v, _ := Get3(big.ParseFloat("1.2345678", 10, 1000, big.ToNearestEven))
+	v, _ := must.Get3(big.ParseFloat("1.2345678", 10, 1000, big.ToNearestEven))
 	fmt.Println(v)
 	// Output: 1.2345678
 }
